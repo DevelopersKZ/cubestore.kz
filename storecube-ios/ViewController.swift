1752353c9cbf93ffa3425aaa0a421788beb80496
Ravil <star.toni@mail.ru>
2023-07-04 11:56:40 +0600

    Initial Commit

---
+++ storecube-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storecube-ios
+//
+//  Created by Ravil on 04.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
