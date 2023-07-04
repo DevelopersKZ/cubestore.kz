07566a43043c5f33d6b7b3b2b09f6f46e34da97f
Radmiry <telmanradmir@gmail.com>
2023-07-04 22:37:34 +0600

    [CK-2] Верстка страницы "Авторизация"

---
+++ storecube-ios/SignIn/SignInController.swift
@@ -0,0 +1,118 @@
+//
+//  SignInController.swift
+//  storecube-ios
+//
+//  Created by Радмир Тельман on 04.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+class SignInController: UIViewController {
+    
+    // MARK: - UI
+    
+    private lazy var signInLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign In"
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        return label
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "E-mail"
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        return textField
+    }()
+    
+    private lazy var signInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign In", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = UIColor(
+            red: CGFloat(0x93) / 255.0,
+            green: CGFloat(0xC2) / 255.0,
+            blue: CGFloat(0xFD) / 255.0,
+            alpha: 1.0
+        )
+        button.layer.cornerRadius = 14
+        return button
+    }()
+    
+    private lazy var createButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Create Account", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = UIColor(
+            red: CGFloat(242) / 255.0,
+            green: CGFloat(243) / 255.0,
+            blue: CGFloat(245) / 255.0,
+            alpha: 1.0
+        )
+        button.layer.cornerRadius = 14
+        return button
+    }()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+        view.backgroundColor = .white
+    }
+    
+    //MARK - setupViews
+    
+    private func setupViews() {
+        view.addSubview(signInLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(signInButton)
+        view.addSubview(createButton)
+    }
+
+    
+    //MARK - setupConstraints
+    
+    private func setupConstraints() {
+        signInLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(93)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(signInLabel.snp.bottom).offset(40)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        
+        passwordTextField.snp.makeConstraints{ make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(40)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
+        
+        signInButton.snp.makeConstraints{ make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(100)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(53)
+        }
+        
+        createButton.snp.makeConstraints{ make in
+            make.top.equalTo(signInButton.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+            make.height.equalTo(53)
+        }
+    }
+    
+}
