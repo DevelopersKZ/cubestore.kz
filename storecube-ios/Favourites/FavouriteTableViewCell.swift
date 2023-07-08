6552df2939d089fc1e466d1896436bdff71e227f
Radmiry <telmanradmir@gmail.com>
2023-07-08 23:14:43 +0600

    [CK-4] Создал страницу "Favourites"

---
+++ storecube-ios/Favourites/FavouriteTableViewCell.swift
@@ -0,0 +1,93 @@
+//
+//  FavouriteTableViewCell.swift
+//  storecube-ios
+//
+//  Created by Радмир Тельман on 08.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class FavouriteTableViewCell: UITableViewCell {
+    
+    // MARK: - UI
+    
+    private lazy var productImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "cube")
+        return imageView
+    }()
+    
+    private lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1 x"
+        label.font = UIFont.boldSystemFont(ofSize: 12)
+        return label
+    }()
+        
+    private lazy var productNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Yulong v2 M 3x3"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10$"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+
+    
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+    
+    // MARK: - setupViews
+    
+    private func setupViews() {
+        contentView.addSubview(productImageView)
+        contentView.addSubview(countLabel)
+        contentView.addSubview(productNameLabel)
+        contentView.addSubview(priceLabel)
+
+    }
+    // MARK: - setupConstraints
+    
+    private func setupConstraints() {
+        productImageView.snp.makeConstraints{ make in
+            make.size.equalTo(70)
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(16)
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.top.equalTo(35)
+            make.leading.equalTo(productImageView.snp.trailing).offset(16)
+        }
+        
+        productNameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(countLabel.snp.trailing).offset(20)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.leading.equalTo(100)
+            make.top.equalTo(countLabel.snp.bottom).offset(18)
+        }
+        
+    }
+    
+}
--- storecube-ios/SceneDelegate.swift
+++ storecube-ios/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let signInController = SignInController()
-        let navigationController = UINavigationController(rootViewController: signInController)
+        let FavouritesController = FavouritesController()
+        let navigationController = UINavigationController(rootViewController: FavouritesController)
         window.rootViewController = navigationController
         
         self.window = window
