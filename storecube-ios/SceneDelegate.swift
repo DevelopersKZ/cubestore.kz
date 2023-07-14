cba33f8fec68f94e5a4359c486b8e2d04ad7911c
Ravil <star.toni@mail.ru>
2023-07-14 12:11:37 +0600

    Первоначальный экран Onboarding

--- storecube-ios/SceneDelegate.swift
+++ storecube-ios/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let windowShow = SignUpController()
-        let navigationController = UINavigationController(rootViewController: SignUpController())
+        let windowShow = OnboardingController()
+        let navigationController = UINavigationController(rootViewController: OnboardingController())
         window.rootViewController = navigationController
         
         self.window = window
