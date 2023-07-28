aeb119e4c153496f9d3f25e49ab2abeddc419b04
Radmiry <telmanradmir@gmail.com>
2023-07-28 17:26:31 +0600

    [CK-22] Правки элементов страницы

--- storecube-ios/Favourites/FavouritesController.swift
+++ storecube-ios/Favourites/FavouritesController.swift
@@ -11,15 +11,7 @@ import SnapKit
 final class FavouritesController: UIViewController {
     
     // MARK: - UI
-    
-    private lazy var favouritesLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Favourites"
-        label.textColor = AppColor.black.uiColor
-        label.font = UIFont.systemFont(ofSize: 22)
-        return label
-    }()
-    
+        
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.register(FavouriteTableViewCell.self, forCellReuseIdentifier: "Избранные")
@@ -27,19 +19,6 @@ final class FavouritesController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         return tableView
-    }()
-    
-    private lazy var signOutButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("SIGN OUT", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = UIColor(
-            red: CGFloat(0x93) / 255.0,
-            green: CGFloat(0xC2) / 255.0,
-            blue: CGFloat(0xFD) / 255.0,
-            alpha: 1.0)
-        button.layer.cornerRadius = 14
-        return button
     }()
     
     // MARK: - Lifecycle
@@ -54,39 +33,26 @@ final class FavouritesController: UIViewController {
     // MARK: - setupViews
     
     private func setupViews() {
-        [favouritesLabel, tableView, signOutButton ].forEach {
-            view.addSubview($0)
-        }
+        view.addSubview(tableView)
         view.backgroundColor = AppColor.silver.uiColor
     }
 
     // MARK: - setupConstraints
     
     private func setupConstraints() {
-        favouritesLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalToSuperview().offset(100)
-        }
         
         tableView.snp.makeConstraints { make in
-            make.top.equalTo(favouritesLabel.snp.bottom).offset(40)
-            make.leading.equalToSuperview().offset(16)
+            make.top.equalToSuperview().offset(95)
+            make.leading.equalToSuperview().offset(20)
             make.trailing.equalToSuperview().offset(-20)
-        }
-        
-        signOutButton.snp.makeConstraints { make in
-            make.top.equalTo(tableView.snp.bottom).offset(40)
-            make.leading.equalToSuperview().offset(16)
-            make.trailing.equalToSuperview().offset(-16)
-            make.bottom.equalToSuperview().offset(-100)
-            make.height.equalTo(53)
+            make.bottom.equalToSuperview().offset(-60)
         }
     }
 }
 
 extension FavouritesController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 30
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
