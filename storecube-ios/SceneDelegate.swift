59277db8ae9ef90f3e3e74cc72324e4b5c45e513
Ravil <star.toni@mail.ru>
2023-07-28 19:01:41 +0600

    SceneDelegate

--- storecube-ios/SceneDelegate.swift
+++ storecube-ios/SceneDelegate.swift
@@ -16,7 +16,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = LaunchScreenController()
+        let windowShow = SignInController()
+        let navigationController = UINavigationController(rootViewController: windowShow)
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
